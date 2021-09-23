8867641821af112556bb1d1d7d938d076d21b7f7
Paru871 <mahalo0521@gmail.com>
2021-09-23 23:41:29 +0900

    パス指定機能を修正

--- 08.ls_object/command.rb
+++ 08.ls_object/command.rb
@@ -2,16 +2,16 @@
 # frozen_string_literal: true
 
 class Command
-  def initialize(pathname, options)
+  def initialize(pathname, params)
     @pathname = pathname
-    @options = options
+    @params = params
   end
 
   def ls_print
     pattern = @pathname.join('*')
-    params = @options['a'] ? [pattern, File::FNM_DOTMATCH] : [pattern]
+    params = @params[:dot_match] ? [pattern, File::FNM_DOTMATCH] : [pattern]
     files = Dir.glob(*params).sort
-    files.reverse! if @options['r']
-    @options['l'] ? LongFormat.new(files).print_list : ShortFormat.new(files).print_column
+    files.reverse! if @params[:reverse]
+    @params[:long_format] ? LongFormat.new(files).print_list : ShortFormat.new(files).print_column
   end
 end
--- 08.ls_object/ls_object.rb
+++ 08.ls_object/ls_object.rb
@@ -12,7 +12,18 @@ require_relative 'short_format'
 NUMBER_OF_COLUMNS = 3 # 列数を指定、3列で表示（列数変更可)
 TERMINAL_WIDTH = `tput cols`.chomp.to_i # ターミナルの横幅取得
 
-path = ARGV[1] || '.'
+# p path = ARGV[0] || '.'
+# p pathname = Pathname(path)
+# p options = ARGV.getopts('a', 'r', 'l')
+# Command.new(pathname, options).ls_print
+
+
+opt = OptionParser.new
+params = { long_format: false, reverse: false, dot_match: false }
+opt.on('-l') { |v| params[:long_format] = v }
+opt.on('-r') { |v| params[:reverse] = v }
+opt.on('-a') { |v| params[:dot_match] = v }
+opt.parse!(ARGV)
+path = ARGV[0] || '.'
 pathname = Pathname(path)
-p options = ARGV.getopts('a', 'r', 'l')
-Command.new(pathname, options).ls_print
+Command.new(pathname, **params).ls_print
