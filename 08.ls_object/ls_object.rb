ce2255e901cc0d99ac61af019d56c37e7c43f3ed
Paru871 <mahalo0521@gmail.com>
2021-09-23 23:50:52 +0900

    コメントアウト部分の削除

--- 08.ls_object/ls_object.rb
+++ 08.ls_object/ls_object.rb
@@ -12,11 +12,6 @@ require_relative 'short_format'
 NUMBER_OF_COLUMNS = 3 # 列数を指定、3列で表示（列数変更可)
 TERMINAL_WIDTH = `tput cols`.chomp.to_i # ターミナルの横幅取得
 
-# p path = ARGV[0] || '.'
-# p pathname = Pathname(path)
-# p options = ARGV.getopts('a', 'r', 'l')
-# Command.new(pathname, options).ls_print
-
 opt = OptionParser.new
 params = { long_format: false, reverse: false, dot_match: false }
 opt.on('-l') { |v| params[:long_format] = v }
